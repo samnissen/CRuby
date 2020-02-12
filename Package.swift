0c0c9d4f975fb143dd3cf2984010d9766f54985b
Guillermo Sáenz <g42v@me.com>
2020-02-12 10:49:35 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,13 @@
 // swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "CRuby",
+    products: [
+    ],
+    dependencies: [
+    ],
+    targets: [
+    ]
+)
